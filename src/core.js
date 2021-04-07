3d378ec4d1fffec00102333fe3e037df20cb1367
于辉 <yuhui@ecpark.cn>
2021-04-07 23:09:10 +0800

    feat:增加理解注释

--- src/core.js
+++ src/core.js
@@ -1,7 +1,8 @@
-'use strict';
+'use strict'
 
-var asap = require('asap/raw');
+var asap = require('asap/raw')
 
+//定义空函数
 function noop() {}
 
 // States:
@@ -18,115 +19,123 @@ function noop() {}
 // We don't use symbols or Object.defineProperty to fully hide them
 // because the performance isn't good enough.
 
-
 // to avoid using try/catch inside critical functions, we
 // extract them to here.
-var LAST_ERROR = null;
-var IS_ERROR = {};
+var LAST_ERROR = null
+var IS_ERROR = {} //用来存储错误信息
+
+//获取实例的then
 function getThen(obj) {
   try {
-    return obj.then;
+    return obj.then
   } catch (ex) {
-    LAST_ERROR = ex;
-    return IS_ERROR;
+    LAST_ERROR = ex
+    return IS_ERROR
   }
 }
-
+//执行then方法回调
 function tryCallOne(fn, a) {
   try {
-    return fn(a);
+    return fn(a)
   } catch (ex) {
-    LAST_ERROR = ex;
-    return IS_ERROR;
+    LAST_ERROR = ex
+    return IS_ERROR
   }
 }
+//执行promise构造函数
 function tryCallTwo(fn, a, b) {
   try {
-    fn(a, b);
+    fn(a, b)
   } catch (ex) {
-    LAST_ERROR = ex;
-    return IS_ERROR;
+    LAST_ERROR = ex
+    return IS_ERROR
   }
 }
 
-module.exports = Promise;
+module.exports = Promise
 
+//promise 构造函数
 function Promise(fn) {
   if (typeof this !== 'object') {
-    throw new TypeError('Promises must be constructed via new');
+    throw new TypeError('Promises must be constructed via new')
   }
   if (typeof fn !== 'function') {
-    throw new TypeError('Promise constructor\'s argument is not a function');
+    throw new TypeError("Promise constructor's argument is not a function")
   }
-  this._deferredState = 0;
-  this._state = 0;
-  this._value = null;
-  this._deferreds = null;
-  if (fn === noop) return;
-  doResolve(fn, this);
+  // 存储的实例状态 0代表还未存储 1代表存储了1个 2代表存储了2个
+  this._deferredState = 0
+  // promise的状态 0 代表padding 1代表Fulfilled 2代表Rejected 3代表resolve传入promise实例
+  this._state = 0
+  //Fulfilled的值
+  this._value = null
+  //用来存储调用then后的实例
+  this._deferreds = null
+  if (fn === noop) return //这句是干嘛用的？
+  // 处理Promise的参数
+  doResolve(fn, this)
 }
-Promise._onHandle = null;
-Promise._onReject = null;
-Promise._noop = noop;
+Promise._onHandle = null
+Promise._onReject = null
+Promise._noop = noop
 
-Promise.prototype.then = function(onFulfilled, onRejected) {
+Promise.prototype.then = function (onFulfilled, onRejected) {
   if (this.constructor !== Promise) {
-    return safeThen(this, onFulfilled, onRejected);
+    return safeThen(this, onFulfilled, onRejected)
   }
-  var res = new Promise(noop);
-  handle(this, new Handler(onFulfilled, onRejected, res));
-  return res;
-};
+  var res = new Promise(noop)
+  handle(this, new Handler(onFulfilled, onRejected, res))
+  return res
+}
 
 function safeThen(self, onFulfilled, onRejected) {
   return new self.constructor(function (resolve, reject) {
-    var res = new Promise(noop);
-    res.then(resolve, reject);
-    handle(self, new Handler(onFulfilled, onRejected, res));
-  });
+    var res = new Promise(noop)
+    res.then(resolve, reject)
+    handle(self, new Handler(onFulfilled, onRejected, res))
+  })
 }
 function handle(self, deferred) {
   while (self._state === 3) {
-    self = self._value;
+    self = self._value
   }
   if (Promise._onHandle) {
-    Promise._onHandle(self);
+    Promise._onHandle(self)
   }
   if (self._state === 0) {
     if (self._deferredState === 0) {
-      self._deferredState = 1;
-      self._deferreds = deferred;
-      return;
+      self._deferredState = 1
+      self._deferreds = deferred
+      return
     }
     if (self._deferredState === 1) {
-      self._deferredState = 2;
-      self._deferreds = [self._deferreds, deferred];
-      return;
+      self._deferredState = 2
+      self._deferreds = [self._deferreds, deferred]
+      return
     }
-    self._deferreds.push(deferred);
-    return;
+    self._deferreds.push(deferred)
+    return
   }
-  handleResolved(self, deferred);
+  handleResolved(self, deferred)
 }
 
 function handleResolved(self, deferred) {
-  asap(function() {
-    var cb = self._state === 1 ? deferred.onFulfilled : deferred.onRejected;
+  asap(function () {
+    var cb = self._state === 1 ? deferred.onFulfilled : deferred.onRejected
     if (cb === null) {
       if (self._state === 1) {
-        resolve(deferred.promise, self._value);
+        resolve(deferred.promise, self._value)
       } else {
-        reject(deferred.promise, self._value);
+        reject(deferred.promise, self._value)
       }
-      return;
+      return
     }
-    var ret = tryCallOne(cb, self._value);
+    var ret = tryCallOne(cb, self._value)
     if (ret === IS_ERROR) {
-      reject(deferred.promise, LAST_ERROR);
+      reject(deferred.promise, LAST_ERROR)
     } else {
-      resolve(deferred.promise, ret);
+      resolve(deferred.promise, ret)
     }
-  });
+  })
 }
 function resolve(self, newValue) {
   // Promise Resolution Procedure: https://github.com/promises-aplus/promises-spec#the-promise-resolution-procedure
@@ -134,59 +143,56 @@ function resolve(self, newValue) {
     return reject(
       self,
       new TypeError('A promise cannot be resolved with itself.')
-    );
+    )
   }
   if (
     newValue &&
     (typeof newValue === 'object' || typeof newValue === 'function')
   ) {
-    var then = getThen(newValue);
+    var then = getThen(newValue)
     if (then === IS_ERROR) {
-      return reject(self, LAST_ERROR);
+      return reject(self, LAST_ERROR)
     }
-    if (
-      then === self.then &&
-      newValue instanceof Promise
-    ) {
-      self._state = 3;
-      self._value = newValue;
-      finale(self);
-      return;
+    if (then === self.then && newValue instanceof Promise) {
+      self._state = 3
+      self._value = newValue
+      finale(self)
+      return
     } else if (typeof then === 'function') {
-      doResolve(then.bind(newValue), self);
-      return;
+      doResolve(then.bind(newValue), self)
+      return
     }
   }
-  self._state = 1;
-  self._value = newValue;
-  finale(self);
+  self._state = 1
+  self._value = newValue
+  finale(self)
 }
 
 function reject(self, newValue) {
-  self._state = 2;
-  self._value = newValue;
+  self._state = 2
+  self._value = newValue
   if (Promise._onReject) {
-    Promise._onReject(self, newValue);
+    Promise._onReject(self, newValue)
   }
-  finale(self);
+  finale(self)
 }
 function finale(self) {
   if (self._deferredState === 1) {
-    handle(self, self._deferreds);
-    self._deferreds = null;
+    handle(self, self._deferreds)
+    self._deferreds = null
   }
   if (self._deferredState === 2) {
     for (var i = 0; i < self._deferreds.length; i++) {
-      handle(self, self._deferreds[i]);
+      handle(self, self._deferreds[i])
     }
-    self._deferreds = null;
+    self._deferreds = null
   }
 }
 
-function Handler(onFulfilled, onRejected, promise){
-  this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
-  this.onRejected = typeof onRejected === 'function' ? onRejected : null;
-  this.promise = promise;
+function Handler(onFulfilled, onRejected, promise) {
+  this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null
+  this.onRejected = typeof onRejected === 'function' ? onRejected : null
+  this.promise = promise
 }
 
 /**
@@ -196,18 +202,26 @@ function Handler(onFulfilled, onRejected, promise){
  * Makes no guarantees about asynchrony.
  */
 function doResolve(fn, promise) {
-  var done = false;
-  var res = tryCallTwo(fn, function (value) {
-    if (done) return;
-    done = true;
-    resolve(promise, value);
-  }, function (reason) {
-    if (done) return;
-    done = true;
-    reject(promise, reason);
-  });
+  //done防止重复触发
+  var done = false
+  //tryCallTwo 用于处理并挂载resolve、reject方法
+  //传入3个参数，Promise构造函数本身，resolve回调，reject回调
+  var res = tryCallTwo(
+    fn,
+    function (value) {
+      if (done) return
+      done = true
+      //处理resolve方法
+      resolve(promise, value)
+    },
+    function (reason) {
+      if (done) return
+      done = true
+      reject(promise, reason)
+    }
+  )
   if (!done && res === IS_ERROR) {
-    done = true;
-    reject(promise, LAST_ERROR);
+    done = true
+    reject(promise, LAST_ERROR)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-'use strict';
+'use strict'
 
-module.exports = require('./core.js');
-require('./done.js');
-require('./finally.js');
-require('./es6-extensions.js');
-require('./node-extensions.js');
-require('./synchronous.js');
+module.exports = require('./core.js')
+require('./done.js')
+require('./finally.js')
+require('./es6-extensions.js')
+require('./node-extensions.js')
+require('./synchronous.js')
+
+/*1、promise对象初始状态为pending，在被resolve或reject时，状态变为Fulfilled或Rejected
+ *2、resolve接收成功的数据，reject接收失败或错误的数据
+ *3、Promise对象必须有一个then方法，且接受两个可变函数onFulfilled、onRejected
+ */
